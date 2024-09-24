27d17b8cdc844074cad41334f3f843fc43e7e421
Tanim Tassanim <tanimtassanim235@gmail.com>
2024-09-24 19:41:33 +0600

    function one & two done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
 
-document.getElementById('blog').addEventListener('click', function () {
-    console.log('blog clicked');
-    // window.location.href = ''
-})
+// document.getElementById('blog').addEventListener('click', function () {
+//     console.log('blog clicked');
+//     // window.location.href = ''
+// })
 
+// get input use  function 
+function getInputValueId(id) {
+    return parseFloat(document.getElementById(id).value);
+}
+
+// toFixed function id by function 
+function donateType(amout) {
+    return `${amout.toFixed(2)}`
+}
 
 const noakhaliDonation = document.getElementById('noakhali-donation');
 noakhaliDonation.addEventListener('click', function () {
 
-    const addMoneyDonation = parseFloat(document.getElementById('noakhali-input').value);
+    // const addMoneyDonation = parseFloat(document.getElementById('noakhali-input').value);
+
+
+    // use function one done
+    const addMoneyDonation = getInputValueId('noakhali-input');
+
     const totalBalance = parseFloat(document.getElementById('noakhali-donation-balance').innerText);
 
     if (addMoneyDonation <= 0 || isNaN(addMoneyDonation) || addMoneyDonation == '') {
@@ -23,7 +37,7 @@ noakhaliDonation.addEventListener('click', function () {
         return
     }
     else {
-        document.getElementById('my_modal_5').showModal()
+        document.getElementById('my_modal_5').showModal();
     }
 
     const newBalance = totalBalance + addMoneyDonation;
@@ -43,7 +57,116 @@ noakhaliDonation.addEventListener('click', function () {
     historyDetails.className = 'bg-white p-4 rounded-md border-2 mb-4  border gray-600';
 
     historyDetails.innerHTML = `
-    <p class="text-xl text-gray-500 pt-2"> ${addMoneyDonation} Taka is Donated for ${noakhaliTitle}</p>
+    <p class="text-xl text-gray-500 pt-2"> ${donateType(addMoneyDonation)} Taka is Donated for ${noakhaliTitle}</p>
+    
+    <p class="text-xl text-gray-500 pt-4 pb-8">Date: ${new Date().toString()} </p>
+    `;
+
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyDetails, historyContainer.firstChild);
+})
+
+
+// Cumilla / Feni donation
+
+const cumillaDonation = document.getElementById('cumilla-donation');
+cumillaDonation.addEventListener('click', function () {
+
+    // const addMoneyDonation = parseFloat(document.getElementById('noakhali-input').value);
+
+
+    // use function one done
+    const addMoneyDonation = getInputValueId('cumilla-input');
+
+    const totalBalance = parseFloat(document.getElementById('cumilla-donation-balance').innerText);
+
+    if (addMoneyDonation <= 0 || isNaN(addMoneyDonation) || addMoneyDonation == '') {
+        alert('Invalid Donation Amount');
+        return;
+    }
+
+    const remainingAccountBalance = parseFloat(document.getElementById('account-balance').innerText);
+
+    if (addMoneyDonation > remainingAccountBalance) {
+        alert('Not Saficient Balance');
+        return
+    }
+    else {
+        document.getElementById('my_modal_5').showModal();
+    }
+
+    const newBalance = totalBalance + addMoneyDonation;
+
+    document.getElementById('cumilla-donation-balance').innerText = newBalance.toFixed(2);
+
+
+    const remainingAccountBalanceNumber = remainingAccountBalance - addMoneyDonation;
+
+
+    document.getElementById('account-balance').innerText = remainingAccountBalanceNumber.toFixed(2);
+
+    const cumillaTitle = document.getElementById('cumilla-title').innerText;
+
+    // History button element
+    const historyDetails = document.createElement('div');
+    historyDetails.className = 'bg-white p-4 rounded-md border-2 mb-4  border gray-600';
+
+    historyDetails.innerHTML = `
+    <p class="text-xl text-gray-500 pt-2"> ${donateType(addMoneyDonation)} Taka is Donated for ${cumillaTitle}</p>
+    
+    <p class="text-xl text-gray-500 pt-4 pb-8">Date: ${new Date().toString()} </p>
+    `;
+
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyDetails, historyContainer.firstChild);
+});
+
+
+// Quota / Health Section 
+const healthDonation = document.getElementById('health-donation');
+healthDonation.addEventListener('click', function () {
+
+    // const addMoneyDonation = parseFloat(document.getElementById('noakhali-input').value);
+
+
+    // use function one done
+    const addMoneyDonation = getInputValueId('health-input');
+
+    const totalBalance = parseFloat(document.getElementById('noakhali-donation-balance').innerText);
+
+    if (addMoneyDonation <= 0 || isNaN(addMoneyDonation) || addMoneyDonation == '') {
+        alert('Invalid Donation Amount');
+        return;
+    }
+
+    const remainingAccountBalance = parseFloat(document.getElementById('account-balance').innerText);
+
+    if (addMoneyDonation > remainingAccountBalance) {
+        alert('Not Saficient Balance');
+        return
+    }
+    else {
+        document.getElementById('my_modal_5').showModal();
+    }
+
+    const newBalance = totalBalance + addMoneyDonation;
+
+    document.getElementById('health-donation-balance').innerText = newBalance.toFixed(2);
+
+
+    const remainingAccountBalanceNumber = remainingAccountBalance - addMoneyDonation;
+
+
+    document.getElementById('account-balance').innerText = remainingAccountBalanceNumber.toFixed(2);
+
+    const healthTitle = document.getElementById('health-title').innerText;
+
+    // History button element
+    const historyDetails = document.createElement('div');
+    historyDetails.className = 'bg-white p-4 rounded-md border-2 mb-4  border gray-600';
+
+    historyDetails.innerHTML = `
+    <p class="text-xl text-gray-500 pt-2"> ${donateType(addMoneyDonation)} Taka is Donated for ${healthTitle}</p>
     
     <p class="text-xl text-gray-500 pt-4 pb-8">Date: ${new Date().toString()} </p>
     `;
